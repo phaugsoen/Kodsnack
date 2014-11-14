5d30f9e49f09bc244b87ad8535ee3c76276e8428
Per Haugsöen <per@haugsoen.se>
2014-11-14 10:21:37 +0100

    Initial Commit

---
+++ Kodsnack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kodsnack
+//
+//  Created by Per Haugsoen on 2014-11-14.
+//  Copyright (c) 2014 Haugsoen App Development AB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+

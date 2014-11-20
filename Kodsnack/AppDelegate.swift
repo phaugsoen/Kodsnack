3303ecb151ba04718a865cd8a9268e9e193ac636
Per Haugsöen <per@haugsoen.se>
2014-11-20 14:55:50 +0100

    asd

--- Kodsnack/AppDelegate.swift
+++ Kodsnack/AppDelegate.swift
@@ -33,23 +33,27 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     
+    // Debug printing
     println("resign active")
   }
 
   func applicationDidEnterBackground(application: UIApplication) {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+     // Debug printing
     println("did enter background")
   }
 
   func applicationWillEnterForeground(application: UIApplication) {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-        println("will enter foreground")
+    // Debug printing
+    println("will enter foreground")
   }
 
   func applicationDidBecomeActive(application: UIApplication) {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-        println("did become Active")
+    // Debug printing
+    println("did become Active")
   }
 
   func applicationWillTerminate(application: UIApplication) {
